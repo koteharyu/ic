054fb23593ad4268c6bb9f57bfc8d4c236267692
haryu <hryau6@gmail.com>
2021-08-17 17:03:01 +0900

    add: 投稿作成機能の実装。画像はまだ

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to posts_path, success: '投稿しました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
+end
