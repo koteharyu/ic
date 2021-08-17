5c2ea6cdceb3ce1d5d26cc04c863c9f18d3d7ef6
haryu <hryau6@gmail.com>
2021-08-17 17:05:00 +0900

    add: ユーザー作成の処理の中にオートログインを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      auto_login(@user)
       redirect_to login_path, success: "ようこそ、#{@user.username}さん"
     else
       flash.now[:danger] = "ユーザーの作成に失敗しました"
