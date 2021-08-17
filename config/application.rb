07f0e4b360aa10035607632299f7344e317556cd
haryu <hryau6@gmail.com>
2021-08-17 13:51:00 +0900

    add: i18nの設定

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,15 @@ module Ic
     # 自動生成されるファイルをerbからslim形式に変更
     config.generators.template_engine = :slim
 
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
+
+    # デフォルトのロケールを日本語に設定
+    config.i18n.default_locale = :ja
+
+    # i18nの複数ロケールファイルが読み込まれるようpathを通す
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
