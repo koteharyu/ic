c4071f4d15b7f24b5d07fc1e62ed4ed14b48f18d
haryu <hryau6@gmail.com>
2021-08-21 11:22:05 +0900

    add: コメントに対するルーティングを追加。shallowオプションを使うことで不要なpost_idを使用しないように

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+# == Route Map
+#
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
@@ -5,5 +8,7 @@ Rails.application.routes.draw do
   post '/login', to: 'user_sessions#create'
   delete '/logout', to: 'user_sessions#destroy'
   resources :users, only: [:new, :create]
-  resources :posts
+  resources :posts do
+    resources :comments, shallow: true
+  end
 end
