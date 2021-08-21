3567186bd60580e26f61fe34471b452cd5dcd4fc
haryu <hryau6@gmail.com>
2021-08-21 11:06:11 +0900

    add: seedデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+15.times do |i|
+  User.create!(username: "user #{i}", email: "user#{i}@example.com", password: "password", password_confirmation: "password")
+end
+
+user = User.first
+
+22.times do |i|
+  user.posts.create!(body: "sample body #{i}")
+end
